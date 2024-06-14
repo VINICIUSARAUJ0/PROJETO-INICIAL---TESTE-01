528ccf2300f283641c29238049706afafd5029d3
Vinícius <mvaraujo>
2024-06-14 18:13:12 -0300

    JS ALT img changing

--- script.js
+++ script.js
@@ -8,4 +8,8 @@ function toggleMode() {
   } else {
     img.setAttribute("src", "./assets/avatar-brownblack.png")
   }
+  // if (html.classList.contais("light")){
+  //   img.setAttribute("alt", "minha neguinha, deitada de óculos, mandando um carinhoso e gostoso beijo!")}
+  // else{
+  //   img.setAttribute("alt", "minha neguinha com soninho, no sofá de óculos e sem camisa!")
 }
