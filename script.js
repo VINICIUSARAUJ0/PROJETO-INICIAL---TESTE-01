230994757b714a67761fe5ee227c6703120955e0
Vinícius <mvaraujo>
2024-06-17 11:42:25 -0300

    changing images and name at profile

--- script.js
+++ script.js
@@ -4,14 +4,21 @@ function toggleMode() {
 
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-brownlight.png")}
-  else {
+    img.setAttribute("src", "./assets/avatar-brownlight.png")
+  } else {
     img.setAttribute("src", "./assets/avatar-brownblack.png")
   }
 
   const image = document.querySelector("#profile img")
-  if (html.classList.contais("light")){
-  image.setAttribute("alt", "minha neguinha, deitada de óculos, mandando um carinhoso e gostoso beijo!")}
-  else{
-  image.setAttribute("alt", "minha neguinha com soninho, no sofá de óculos e sem camisa!")}
+  if (html.classList.contais("light")) {
+    image.setAttribute(
+      "alt",
+      "Williams Almeida, na de óculos de sol, camisa de botão clara, em ambiente praiano."
+    )
+  } else {
+    image.setAttribute(
+      "alt",
+      "Williams Almeida, sentado, com airpods e camisa preta."
+    )
   }
+}
