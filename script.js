498154e4e6511d6cc427169ea27d5d769030e7a4
Vinícius <mvaraujo>
2024-06-17 10:50:50 -0300

    applying animation property in CSS

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ function toggleMode() {
 
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-brownlight.png")
-  } else {
+    img.setAttribute("src", "./assets/avatar-brownlight.png")}
+  else {
     img.setAttribute("src", "./assets/avatar-brownblack.png")
   }
-  // if (html.classList.contais("light")){
-  //   img.setAttribute("alt", "minha neguinha, deitada de óculos, mandando um carinhoso e gostoso beijo!")}
-  // else{
-  //   img.setAttribute("alt", "minha neguinha com soninho, no sofá de óculos e sem camisa!")
-}
+
+  const image = document.querySelector("#profile img")
+  if (html.classList.contais("light")){
+  image.setAttribute("alt", "minha neguinha, deitada de óculos, mandando um carinhoso e gostoso beijo!")}
+  else{
+  image.setAttribute("alt", "minha neguinha com soninho, no sofá de óculos e sem camisa!")}
+  }
